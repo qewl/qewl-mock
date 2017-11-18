ec26e4ee702441bfed5ee992074348b1867b7df9
Kim Brandwijk <kim.brandwijk@gmail.com>
2017-11-18 04:28:07 +0100

    Added DateTime and ID defaults

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import { addMockFunctionsToSchema } from 'graphql-tools'
 import { GraphQLSchema } from 'graphql'
 import { merge } from 'lodash'
 import * as faker from 'faker'
+import * as cuid from 'cuid'
 
 export const mock = ({ schema, mocks = {}, preserveResolvers = false }: { schema: string; mocks: IMocks, preserveResolvers: boolean }) => {
   return (req: any, res: any, next: any): any => {
 
     const defaultMocks = {
+      ID: () => cuid(),
+      DateTime: () => faker.date.recent(),
       Int: () => faker.random.number(100),
       Float: () => faker.random.number({precision: 0.1}),
       String: () => faker.random.words(faker.random.number(4)),
