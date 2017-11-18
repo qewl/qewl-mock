74ec5e5c0d0754c2b3c0547ef4fc1972eaef1ee0
Kim Brandwijk <kim.brandwijk@gmail.com>
2017-11-18 03:30:30 +0100

    Added realistic defaults

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { IMocks } from 'graphql-tools/dist/Interfaces'
 import { addMockFunctionsToSchema } from 'graphql-tools'
 import { GraphQLSchema } from 'graphql'
+import { merge } from 'lodash'
+import * as faker from 'faker'
 
 export const mock = ({ schema, mocks }: { schema: string; mocks: IMocks }) => {
   return (req: any, res: any, next: any): any => {
+
+    const defaultMocks = {
+      Int: () => faker.random.number(100),
+      Float: () => faker.random.number({precision: 0.1}),
+      String: () => faker.random.words(faker.random.number(4)),
+    }
+
+    mocks = merge(defaultMocks, mocks)
 
     if (req.qewl.schemas[schema] instanceof GraphQLSchema) {
       addMockFunctionsToSchema({ schema: req.qewl.schemas[schema], mocks })
